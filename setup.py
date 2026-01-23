172abbbbcda9e47af94cc2c48704f9c60ddb2490
Некрасов Павел Игоревич <pinekrasov@navio.auto>
2026-01-23 15:16:04 +0300

    Fix ruff linting errors in setup.py
    - Fix import order (standard library imports before third-party)
    - Add trailing newline at end of file

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 """Setup configuration for uuidv7 package."""
+
 import os
+
 from setuptools import Extension, setup
 
-
 uuidv7_extension = Extension(
-    'uuidv7.uuidv7_impl.uuid7_gen',
+    "uuidv7.uuidv7_impl.uuid7_gen",
     sources=[
-        os.path.join('uuidv7', 'uuidv7_impl', 'uuid7_gen.c'),
-        os.path.join('uuidv7', 'uuidv7_impl', 'src', 'uuid7_gen.c'),
+        os.path.join("uuidv7", "uuidv7_impl", "uuid7_gen.c"),
+        os.path.join("uuidv7", "uuidv7_impl", "src", "uuid7_gen.c"),
     ],
-    include_dirs=[
-        os.path.join('uuidv7', 'uuidv7_impl', 'include')
-    ],
-    libraries=['rt'],  # For clock_gettime on Linux
+    include_dirs=[os.path.join("uuidv7", "uuidv7_impl", "include")],
+    libraries=["rt"],  # For clock_gettime on Linux
 )
 
 
